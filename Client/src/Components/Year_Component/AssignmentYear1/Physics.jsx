3814b6bc77c8f6cb4848f4599af42bd1aa3afa6a
Abhishek <abhisheksonparote6@gmail.com>
2026-06-29 18:37:24 +0530

    Fix chat deployment

--- Client/src/Components/Year_Component/AssignmentYear1/Physics.jsx
+++ Client/src/Components/Year_Component/AssignmentYear1/Physics.jsx
@@ -75,10 +75,8 @@ export default function Physics() {
 
 useEffect(() => {
 
-    // Load all previous messages from MySQL
     loadMessages();
 
-    // Listen for new messages
     socket.on("receive_message", (newMessage) => {
 
         if (
@@ -102,7 +100,7 @@ useEffect(() => {
     try {
 
         const response = await fetch(
-            "http://localhost:5000/chat/send",
+            `${import.meta.env.VITE_API_URL}/chat/send`,
             {
                 method: "POST",
 
